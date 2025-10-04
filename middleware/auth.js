2a27280ed3f9b106bfd84bafa7f8e842c1e64c6e
raghavsharma098 <raghavrock098@gmail.com>
2025-10-05 01:41:03 +0530

    done

--- middleware/auth.js
+++
@@ -1,8 +0,0 @@
-const authMiddleware = (req, res, next) => {
-    if (!req.session.companyId) {
-        return res.redirect('/auth/login');
-    }
-    next();
-};
-
-module.exports = authMiddleware;
